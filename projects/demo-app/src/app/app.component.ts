2043479801a5df3adb597ae295bef34f19fa6e35
Gihan Ediriweera Jayakuru <gihan22603@gmail.com>
2024-02-17 17:04:29 +0530

    doctoc for readme

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxIntlTelInputComponent } from '../../../ngx-intl-tel-input/src/public-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxIntlTelInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'demo-app';
